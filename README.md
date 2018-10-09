7e55775bb5ac9cc97d771492894c05023dd1d1e4
Shrinivasan <tshrinivasan@gmail.com>
2018-10-09 07:10:02 +0530

    workin in progress

--- README.md
+++ README.md
@@ -1,99 +1,19 @@
-# Version
-1.54
+# text_detection_python (with Google Vision API)
 
-#Note
-This program is evolving heavily.
-Always use the latest version.
-Compare the version number with your's version.
+>>This script will convert your multi pages PDF file to single pages PDF
 
-Feel free to report any errors in the issues section.
+>>Convert the single page PDF to Image
 
+>>Recognize the text from the image and save the text to a TXT file.
 
-# OCR4wikisource
+* Import VISION_API to your local machine
 
+* pip install -r requirements.txt
 
-There are many PDF files and DJVU files in WikiSource in various languages.
-In many wikisource projects, those files are splited into individual page as an Image, using proofRead extension.
+* Create Folders named pdf, image, text to the current folder
 
-Contributors see those images and type them manually.
+* place your pdf file the pdf folder
 
-This project helps the wikisource team to OCR the entire PDF or DJVU file, using the google drive OCR.
-Then it will update the relevant page in the wikisource with the text.
+* run the script ./pdf_img_text.py
 
-The OCR will not give 100% correct text. But still, it is better than typing entire page manually.
-
-To run this, you need a GNU/Linux system. Sorry Windows guys. It uses the commandline utilities available in GNU/Linux heavily.
-
-
-Check the file INSTALL for installation instructions
-
-
-
-config.ini
-==========
-
-Edit this file and fill the details as file_url, columns, wiki_usernmae, wiki_password, wikisource_language_code
-
-do_ocr.py
-========
-
-Run this as
-
-```
-python do_ocr.py
-```
-
-This will do the following things.
-
-* download the file
-* if it is DJVU, convert to PDF
-* split it into individual files based on column no
-* create a folder in your Google Drive
-* upload the PDF files to Google Drive
-* Download the OCRed text
-* split, merge the text files properly to fit as the PDF files
-
-
-This will give text files as 'text_for_page_00001.txt' etc equvalent to the no of pages in the PDF file.
-
-
-mediawiki_uploader.py
-=====================
-
-run as
-
-```
-python mediawiki_uploader.py
-```
-
-This uploads the text files provided by do_ocr.py to the wikisource details you provided in config.ini
-
-For testing you can keep only few files provided by do_ocr.py (example from text_for_page_00001.txt to text_for_page_00005.txt)
-Move all other text files to another folder.
-Once you are satisfied, you can place all the files in the current folder.
-
-
-For this, the PDF or DJVU file should be already splitted into individual pages in wiki souce, using Proofread extension for wikisource.
-
-```
-Example :  "https://" + wikisource_language_code + ".wikisource.org/wiki/Page:" + filename/pageno
-```
-
-Contact your wikisource team for splitting the files into individual pages.
-
-
-
-
-
-ToDo:
-====
-
-
-* This program uplads single page PDF to google drive. Google accepts 10 page PDF files. Create 10 page pdf files, upload, split the text files for faster operations.
-* Give a web interface using Django or Flask.
-* Port to other Operating Systems like Windows.
-
-
-
-
-Contact : T Shrinivasan <tshrinivasan@gmail.com>
+* For sample, added a file named "code.pdf"
--- pdf2text.py
+++ pdf2text.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
 import requests
@@ -13,6 +13,11 @@ import urllib
 import logging
 import urllib2
 import os.path
+from utils import Service, encode_image
+
+
+
+
 
 
 version = "1"
@@ -33,11 +38,11 @@ timestamp  = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
 
 
 if not os.path.isdir("./log"):
-            os.mkdir("./log")
+    os.mkdir("./log")
 
 
 # create a file handler
-log_file = './log/do_ocr_' + timestamp + '_log.txt'
+log_file = './log/pdf2text_' + timestamp + '_log.txt'
 
 handler = logging.FileHandler(log_file)
 handler.setLevel(logging.INFO)
@@ -53,11 +58,11 @@ logger.addHandler(handler)
 
 
 
-latest_version =  urllib2.urlopen('https://raw.githubusercontent.com/tshrinivasan/OCR4wikisource/master/VERSION').read().strip('\n').split(' ')[1]
+latest_version =  urllib2.urlopen('https://raw.githubusercontent.com/KaniyamFoundation/Pdf2Text/master/VERSION').read().strip('\n').split(' ')[1]
 
 if not float(version) == float(latest_version):
-            logger.info("\n\nYour pdf2text version is " + version + ". This is old. The latest version is " + latest_version + ". Update from https://github.com/tshrinivasan/OCR4wikisource \n\n")
-            sys.exit()
+    logger.info("\n\nYour pdf2text version is " + version + ". This is old. The latest version is " + latest_version + ". Update from https://github.com/KaniyamFoundation/Pdf2Text \n\n")
+    sys.exit()
 
 
 
@@ -78,177 +83,135 @@ logging.info("Operating System = " + os_version)
 
 #Read the config file
 
-url = config.get('settings','file_url')
+input_filename = config.get('settings','file_name').replace(' ','_')
+input_folder = config.get('settings','folder_name')
 columns = config.get('settings','columns')
-keep_temp_folder_in_google_drive = config.get('settings','keep_temp_folder_in_google_drive')
-#start_page = config.get('settings','start_page')
-#end_page = config.get('settings','end_page')
 
 
 
-logger.info("URL = " + url )
+logger.info("File name = " + input_filename )
+logger.info("Folder name = " + input_folder )
 logger.info("Columns = " + columns )
-logger.info("Keep Temp folder in  Google Drive = " + keep_temp_folder_in_google_drive)
-#logger.info("Start Page = " + str(start_page))
-#logger.info("End Page = " + str(end_page))
 
 
 
+filetype = input_filename.split('.')[-1].lower()
 
-original_url = urllib.unquote(url).decode('utf8')
-
-logger.info("Original URL = " + original_url )
-
-
-filename = os.path.basename(original_url)
-filetype = filename.split('.')[-1].lower()
-
-logger.info("File Name = " + filename)
 logger.info("File Type = " + filetype)
 
 
-temp_folder = "OCR-" + filename + '-temp-'+ timestamp
+temp_folder = "OCR-" + input_filename + '-temp-'+ timestamp
 logger.info("Created Temp folder " + temp_folder)
 
 if not os.path.isdir(temp_folder):
-      os.mkdir(temp_folder)
+    os.mkdir(temp_folder)
                             
-
-
-
-if os.path.isfile(filename):
-            logging.info(filename + " Already Exists. Skipping the download.")
-
-else:
-            print "\n\nDownloading the file " + filename + "\n\n"
-
-            logger.info("Downloading the file " + filename )
-
-            #Download the file
-
-            r = requests.get(url, stream=True)
-            with open(filename, 'wb') as f:
-                        total_length = int(r.headers.get('content-length'))
-                        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
-                                    if chunk:
-                                                f.write(chunk)
-                                                f.flush()
-
-
-            logger.info("Download Completed")
-
-
-
-
-# Convert djvu to PDF
-
-if filetype.lower() == "djvu" :
-
-            if os.path.isfile(filename.split('.')[0] + ".pdf"):
-                        logging.info("Found PDF version. Skipping DJVU to PDF conversion")
-                        filename = filename.split('.')[0] + ".pdf"
-                        filetype = filename.split('.')[-1].lower()
-                                                        
-            else:
-                        
-                        message  =  "Found a djvu file. Converting to PDF file. " + "\n\n"
-                        logger.info(message)
-        
-                        command = "ddjvu --format=pdf " +  '"' + filename +  '"' + "   " + '"' +  filename.split('.')[0] + '"' + ".pdf"
-                        os.system(command.encode('utf-8'))
-                        logger.info("Running " + command)
-
-                        filename = filename.split('.')[0] + ".pdf"
-                        filetype = filename.split('.')[-1].lower()
-        
-
 
 if filetype.lower() == "pdf":
 
 	# split the PDF files vertically based on the column numbers
 
-        message =  "Aligining the Pages of PDF file. \n"
-        logger.info(message)
-        command = "mutool poster -x " + str(columns)  + " " + '"' +  filename + '"' +  "  currentfile.pdf"
-        logger.info("Running " + command.encode('utf-8'))
+    message =  "Aligining the Pages of PDF file. \n"
+    logger.info(message)
+    command = "mutool poster -x " + str(columns)  + " " + '"' +  input_filename + '"' +  "  currentfile.pdf"
+    logger.info("Running " + command.encode('utf-8'))
         
-        os.system(command.encode('utf-8'))
+    os.system(command.encode('utf-8'))
                 
 
-        message =  "Spliting the PDF into single pages. \n"
-        logger.info(message)
-        burst_command = "pdftk currentfile.pdf burst"
-        os.system(burst_command)
-        logger.info("Running " + burst_command) 
+# https://blog.alivate.com.au/poppler-windows/
+
+    message =  "Spliting the PDF into single pages. \n"
+    logger.info(message)
+    burst_command = "pdfseparate currentfile.pdf pg-%05d.pdf"
+    os.system(burst_command)
+    logger.info("Running " + burst_command) 
 
         
-        files = []
-        for filename in glob.glob('pg*.pdf'):
-                files.append(filename)
-                files.sort()
+    files = []
+    for filename in glob.glob('pg*.pdf'):
+        files.append(filename)
+        files.sort()
 
-        chunks=[files[x:x+int(columns)] for x in xrange(0, len(files), int(columns))]
+    chunks=[files[x:x+int(columns)] for x in xrange(0, len(files), int(columns))]
 
+    counter = 1
+    message =  "Joining the PDF files ...\n"
+    logger.info(message)
+
+    if columns == "1":
         counter = 1
-        message =  "Joining the PDF files ...\n"
-        logger.info(message)
-
-        if columns == "1":
-                counter = 1
-                for pdf in files:
-                        command = "cp " + pdf +  " page_" + str(counter).zfill(5) + ".pdf"
-                        logger.info("Running Command " + command)
-                        counter = counter + 1
-                        os.system(command)
+        for pdf in files:
+            command = "cp " + pdf +  " page_" + str(counter).zfill(5) + ".pdf"
+            logger.info("Running Command " + command)
+            counter = counter + 1
+            os.system(command)
 
 
-        if columns == "2":
+    if columns == "2":
 
-	        chunks=[files[x:x+int(columns)] for x in xrange(0, len(files), int(columns))]
+	    chunks=[files[x:x+int(columns)] for x in xrange(0, len(files), int(columns))]
 
-	        counter = 1
-	        message =  "Joining the PDF files ...\n"
-	        logger.info(message)
+	    counter = 1
+	    message =  "Joining the PDF files ...\n"
+	    logger.info(message)
 	        
-	        for i in chunks:
-	            com =  ' '.join(i)
-	            command = "pdfunite " + com + " " + "page_" + str(counter).zfill(5) + ".pdf"
-	            logger.info("Running " + command) 
-	            counter = counter + 1
-	            os.system(command)
+	    for i in chunks:
+	        com =  ' '.join(i)
+	        command = "pdfunite " + com + " " + "page_" + str(counter).zfill(5) + ".pdf"
+	        logger.info("Running " + command) 
+	        counter = counter + 1
+	        os.system(command)
                                 
 
         
 
 def move_file(file):
-        source = file
-        destination = temp_folder
+    source = file
+    destination = temp_folder
 
-        if os.path.isdir(temp_folder):
-                shutil.move(source,destination)
-        else:
-                os.mkdir(temp_folder)
-                shutil.move(source,destination)
+    if os.path.isdir(temp_folder):
+        shutil.move(source,destination)
+    else:
+        os.mkdir(temp_folder)
+        shutil.move(source,destination)
         message =  "Moving the file " + file + " to the folder " + temp_folder + "\n"
         logger.info(message)
 
 
+def get_text(photo_file):
+    """Run a text detection request on a single image"""
 
+    access_token = os.environ.get('VISION_API')
+    print(access_token)
+    if access_token == 'None':
+        print("Import VISION API KEY")
+        sys.exit()
 
-# Create a temp folder in google drive to upload the files. You can delete this folder later.
+    service = Service('vision', 'v1', access_token=access_token)
+    with open(photo_file, 'rb') as image:
+        base64_image = encode_image(image)
+        body = {
+            'requests': [{
+                'image': {
+                    'content': base64_image,
+                },
+                'features': [{
+                    'type': 'TEXT_DETECTION',
+                    'maxResults': 1,
+                }]
 
-logger.info( "\nCreating a folder in Google Drive to upload files. Folder Name : " + temp_folder + "\n\n")
+            }]
+        }
+        response = service.execute(body=body)
+        #print(response)
+        if response['responses'][0]:
+            text = response['responses'][0]['textAnnotations'][0]['description']
+            #print('Found text: {}'.format(text))
+        else:
+            text = " "
 
-create_folder_in_drive_command = "gdmkdir.py " + '"' +  temp_folder + '"'  + " | tee folder_in_google_drive.log"
-logger.info("Running " + create_folder_in_drive_command.encode('utf-8'))
-os.system(create_folder_in_drive_command.encode('utf-8'))
-
-
-resultfile = open("folder_in_google_drive.log","r").readlines()
-
-for line in resultfile:
-        if "id:" in line:
-                drive_folder_id = line.split(":")[1].strip()
+    return text
 
                 
 
@@ -262,23 +225,7 @@ for pdf in glob.glob('page_*.pdf'):
  
 files = []
 for filename in glob.glob('page_*.jpg'):
-            files.append(filename)
-
-
-
-
-
-#pages = []
-#if not end_page == "END" :
-#    for pageno in range(int(start_page), int(end_page) + 1):
-#         pages.append("page_" + str(pageno).zfill(5) + ".pdf")   
-
-#if end_page == "END":
-#    for pageno in range(int(start_page),len(files) +1):
-#        pages.append("page_" + str(pageno).zfill(5) + ".pdf")
-        
-
-
+    files.append(filename)
 
 
 
@@ -293,39 +240,26 @@ for jpg_file in sorted(files):
     if not os.path.isfile(jpg_file.split('.')[0] + ".upload"):                        
                                                 
 
-            message= "\n\nuploading " + jpg_file + " to google Drive. "     # File " + str(upload_counter) + " of " + str(len(files)) + " \n\n"
-            logger.info(message)
-            command = "gdput.py -t ocr -f   " +  drive_folder_id + " "  + jpg_file + " | tee " + jpg_file.split('.')[0] + ".log"
-            
-            logger.info("Running " + command)
-            os.system(command)
+        message= "\n\nuploading " + jpg_file + " to google. "     # File " + str(upload_counter) + " of " + str(len(files)) + " \n\n"
+        logger.info(message)
 
+        text = get_text(jpg_file)   
+
+        filename = jpg_file.split(".")[0] + ".txt"
+
+        file1=open(filename,"a")
+        #file1.write(text,'\n')
+        file1.write("{}\n".format(text))
+        file1.close()
                                                             
-
-            resultfile = open(jpg_file.split('.')[0] + ".log","r").readlines()
-
-            for line in resultfile:
-                        if "id:" in line:
-                                    fileid = line.split(":")[1].strip()
-                                    filename = jpg_file.split(".")[0] + ".txt"
-                                    get_command = "gdget.py -f txt -s " + filename + " " + fileid
-                                    logger.info( "\n\nDownloading the OCRed text \n\n ")
-                                    logger.info("Running " + command)
-                              #      os.system(get_command)
-
-
-
-                                    download_status = os.system(get_command)
-
-                                    if download_status == 0:
-                                                create_temp_file = "touch " + jpg_file.split('.')[0] + ".upload"
-                                                logger.info("\n  Creating temp file " + create_temp_file + "\n")
-                                                os.system(create_temp_file)
+        create_temp_file = "touch " + jpg_file.split('.')[0] + ".upload"
+        logger.info("\n  Creating temp file " + create_temp_file + "\n")
+        os.system(create_temp_file)
                         
 
 #   	    move_file(pdf_file)
-            logger.info( "\n\n========\n\n")
-            upload_counter = upload_counter + 1
+        logger.info( "\n\n========\n\n")
+        upload_counter = upload_counter + 1
 
 
 
@@ -341,7 +275,7 @@ if not jpg_count == text_count:
             for i in range(1,int(jpg_count)+1):
                         txt_file = "page_" + str(i).zfill(5) + ".txt"
                         if not os.path.isfile(txt_file):
-				    missing_files.write(txt_file +"\n")
+                                    missing_files.write(txt_file +"\n")
                                     logger.info( "Missing " + txt_file)
                                     logger.info( "page_" + str(i).zfill(5) + ".jpg" + " should be reuploaded ")
                                                                          
@@ -376,7 +310,7 @@ if int(columns)==1:
                                  i = i+1
                          else:
 
-				with open('txt_'+str(i).zfill(5)+'.txt', 'w') as towrite:
+                                with open('txt_'+str(i).zfill(5)+'.txt', 'w') as towrite:
                                 	towrite.write(' ')
                                 i = i+1
 
@@ -390,10 +324,10 @@ elif int(columns)==2:
     for textfile in files:
         with open(textfile,'r') as filetosplit:
                 content = filetosplit.read()
-		if "________________" in content:
-                	records = content.split('________________')
-                	print records
-                	for record in records[1::2]:
+                if "________________" in content:
+                        records = content.split('________________')
+                        #print records
+                        for record in records[1::2]:
                                 with open('txt_'+str(i).zfill(5)+'.txt', 'w') as towrite:
                                         towrite.write(record)
                                 i = i+1
@@ -437,7 +371,6 @@ os.system(command.encode('utf-8'))
 
 
 
-original_filename = os.path.basename(original_url)
 
 files = []
 for textfile in glob.glob('text_for_page*.txt'):
@@ -445,7 +378,7 @@ for textfile in glob.glob('text_for_page*.txt'):
             files.sort()
 
             
-single_file = open("all_text_for_" + original_filename + ".txt" ,"w")
+single_file = open("all_text_for_" + filename + ".txt" ,"w")
 
 counter = 1
 for filename in files:
@@ -461,14 +394,14 @@ for filename in files:
 single_file.close()
                                                                 
 
-logger.info("Merged all OCRed files to  all_text_for_" + original_filename + ".txt")
+logger.info("Merged all OCRed files to  all_text_for_" + filename + ".txt")
 
 
-if not os.path.isdir("text-for-" + original_filename):
-            os.mkdir("text-for-" + original_filename)
+if not os.path.isdir("text-for-" + filename):
+            os.mkdir("text-for-" + filename)
 
-command = "cp *.txt text-for-" + original_filename
-logger.info("Making a copy of all text files to text-for-" + original_filename)
+command = "cp *.txt text-for-" + filename
+logger.info("Making a copy of all text files to text-for-" + filename)
 logger.info("Running " + command.encode('utf-8'))
 os.system(command.encode('utf-8'))
 
@@ -496,10 +429,6 @@ if not jpg_count == result_text_count:
             logger.info(" \n\nText files are not equal to JPG files. Some JPG files not OCRed. Run this script again to complete OCR all the JPG     files \n\n")
             sys.exit()
 
-#if  pdf_count == result_text_count:
-#            logger.info("\n\nThe PDF files and result text files are equval. Now running the mediawiki_uploader.py script\n\n")
-#            command = "python mediawiki_uploader.py"
-#            os.system(command)
-            
+
 
                                         
