17b0c15ed0e6c2dcc7f3e24efe0cb5dace315527
Shrinivasan <tshrinivasan@gmail.com>
2018-10-27 05:28:09 +0530

    fixed binary paths

--- pdf2text.py
+++ pdf2text.py
@@ -12,11 +12,9 @@ import configparser
 import logging
 import os.path
 from utils import Service, encode_image
-#from urllib.request import urlopen
+
+# from urllib.request import urlopen
 import requests
-
-
-
 
 
 version = "1"
@@ -31,9 +29,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-
 ts = time.time()
-timestamp  = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+timestamp = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d-%H-%M-%S")
 
 
 if not os.path.isdir("./log"):
@@ -41,14 +38,14 @@ if not os.path.isdir("./log"):
 
 
 # create a file handler
-log_file = './log/pdf2text_' + timestamp + '_log.txt'
+log_file = "./log/pdf2text_" + timestamp + "_log.txt"
 
 handler = logging.FileHandler(log_file)
 handler.setLevel(logging.INFO)
 
 # create a logging format
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 
 # add the handlers to the logger
@@ -56,16 +53,26 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-latest_version = requests.get('https://raw.githubusercontent.com/KaniyamFoundation/Pdf2Text/master/VERSION').text.strip('\n').split(' ')[1]
+latest_version = (
+    requests.get(
+        "https://raw.githubusercontent.com/KaniyamFoundation/Pdf2Text/master/VERSION"
+    )
+    .text.strip("\n")
+    .split(" ")[1]
+)
 
 print(latest_version)
 
 
 if not float(version) == float(latest_version):
-    logger.info("\n\nYour pdf2text version is " + version + ". This is old. The latest version is " + latest_version + ". Update from https://github.com/KaniyamFoundation/Pdf2Text \n\n")
+    logger.info(
+        "\n\nYour pdf2text version is "
+        + version
+        + ". This is old. The latest version is "
+        + latest_version
+        + ". Update from https://github.com/KaniyamFoundation/Pdf2Text \n\n"
+    )
     sys.exit()
-
-
 
 
 logger.info("Running pdf2text.py " + version)
@@ -73,236 +80,237 @@ logger.info("Running pdf2text.py " + version)
 
 os_info = open("/etc/lsb-release")
 for line in os_info:
-	if  "DISTRIB_DESCRIPTION" in line:
-		os_version = line.split("=")[1]
+    if "DISTRIB_DESCRIPTION" in line:
+        os_version = line.split("=")[1]
 
 logging.info("Operating System = " + os_version)
 
 
+# Read the config file
+
+input_filename = config.get("settings", "file_name")
+input_folder = config.get("settings", "folder_name")
+columns = config.get("settings", "columns")
+
+MUTOOL = config.get("application_path","mutool")
+PDFSEPARATE = config.get("application_path",  "pdfseparate")
+PDFUNITE = config.get("application_path",  "pdfunite")
+GS = config.get("application_path",  "gs")
 
 
 
-#Read the config file
-
-input_filename = config.get('settings','file_name')
-input_folder = config.get('settings','folder_name')
-columns = config.get('settings','columns')
+logger.info("File name = " + input_filename)
+logger.info("Folder name = " + input_folder)
+logger.info("Columns = " + columns)
 
 
-
-logger.info("File name = " + input_filename )
-logger.info("Folder name = " + input_folder )
-logger.info("Columns = " + columns )
-
-
-
-filetype = input_filename.split('.')[-1].lower()
+filetype = input_filename.split(".")[-1].lower()
 
 logger.info("File Type = " + filetype)
 
 
-temp_folder = "OCR-" + input_filename + '-temp-'+ timestamp
+temp_folder = "OCR-" + input_filename + "-temp-" + timestamp
 logger.info("Created Temp folder " + temp_folder)
 
 if not os.path.isdir(temp_folder):
     os.mkdir(temp_folder)
-                            
+
 
 if filetype.lower() == "pdf":
 
-	# split the PDF files vertically based on the column numbers
+    # split the PDF files vertically based on the column numbers
 
-    message =  "Aligining the Pages of PDF file. \n"
+    message = "Aligining the Pages of PDF file. \n"
     logger.info(message)
-    command = "mutool poster -x " + str(columns)  + " " + '"' +  input_filename + '"' +  "  currentfile.pdf"
+    command = (
+        MUTOOL + " poster -x "
+        + str(columns)
+        + " "
+        + '"'
+        + input_filename
+        + '"'
+        + "  currentfile.pdf"
+    )
     logger.info("Running " + command)
-        
-    os.system(command.encode('utf-8'))
-                
 
-# https://blog.alivate.com.au/poppler-windows/
+    os.system(command.encode("utf-8"))
 
-    message =  "Spliting the PDF into single pages. \n"
+    # https://blog.alivate.com.au/poppler-windows/
+
+    message = "Spliting the PDF into single pages. \n"
     logger.info(message)
-    burst_command = "pdfseparate currentfile.pdf pg-%05d.pdf"
+    burst_command = PDFSEPARATE + " currentfile.pdf pg-%05d.pdf"
     os.system(burst_command)
-    logger.info("Running " + burst_command) 
+    logger.info("Running " + burst_command)
 
-        
     files = []
-    for filename in glob.glob('pg*.pdf'):
+    for filename in glob.glob("pg*.pdf"):
         files.append(filename)
         files.sort()
 
-    chunks=[files[x:x+int(columns)] for x in range(0, len(files), int(columns))]
+    chunks = [files[x : x + int(columns)] for x in range(0, len(files), int(columns))]
 
     counter = 1
-    message =  "Joining the PDF files ...\n"
+    message = "Joining the PDF files ...\n"
     logger.info(message)
 
     if columns == "1":
         counter = 1
         for pdf in files:
-            shutil.copy(pdf,"page_" + str(counter).zfill(5) + ".pdf")
-            #command = "cp " + pdf +  " page_" + str(counter).zfill(5) + ".pdf"
-            #logger.info("Running Command " + command)
+            shutil.copy(pdf, "page_" + str(counter).zfill(5) + ".pdf")
+            # command = "cp " + pdf +  " page_" + str(counter).zfill(5) + ".pdf"
+            # logger.info("Running Command " + command)
             counter = counter + 1
-            #os.system(command)
-
+            # os.system(command)
 
     if columns == "2":
 
-	    chunks=[files[x:x+int(columns)] for x in range(0, len(files), int(columns))]
+        chunks = [
+            files[x : x + int(columns)] for x in range(0, len(files), int(columns))
+        ]
 
-	    counter = 1
-	    message =  "Joining the PDF files ...\n"
-	    logger.info(message)
-	        
-	    for i in chunks:
-	        com =  ' '.join(i)
-	        command = "pdfunite " + com + " " + "page_" + str(counter).zfill(5) + ".pdf"
-	        logger.info("Running " + command) 
-	        counter = counter + 1
-	        os.system(command)
-                                
+        counter = 1
+        message = "Joining the PDF files ...\n"
+        logger.info(message)
 
-        
+        for i in chunks:
+            com = " ".join(i)
+            command = PDFUNITE + " " + com + " " + "page_" + str(counter).zfill(5) + ".pdf"
+            logger.info("Running " + command)
+            counter = counter + 1
+            os.system(command)
+
 
 def move_file(file):
     source = file
     destination = temp_folder
 
     if os.path.isdir(temp_folder):
-        shutil.move(source,destination)
+        shutil.move(source, destination)
     else:
         os.mkdir(temp_folder)
-        shutil.move(source,destination)
-        message =  "Moving the file " + file + " to the folder " + temp_folder + "\n"
+        shutil.move(source, destination)
+        message = "Moving the file " + file + " to the folder " + temp_folder + "\n"
         logger.info(message)
 
 
-def get_text(photo_file):
+def get_text(image_file):
     """Run a text detection request on a single image"""
 
-    access_token = os.environ.get('VISION_API')
+    access_token = os.environ.get("VISION_API")
     print(access_token)
-    if access_token == 'None':
+    if access_token == "None":
         print("Import VISION API KEY")
         sys.exit()
 
-    service = Service('vision', 'v1', access_token=access_token)
-    with open(photo_file, 'rb') as image:
+    service = Service("vision", "v1", access_token=access_token)
+    with open(image_file, "rb") as image:
         base64_image = encode_image(image)
         body = {
-            'requests': [{
-                'image': {
-                    'content': base64_image,
-                },
-                'features': [{
-                    'type': 'TEXT_DETECTION',
-                    'maxResults': 1,
-                }]
-
-            }]
+            "requests": [
+                {
+                    "image": {"content": base64_image},
+                    "features": [{"type": "TEXT_DETECTION", "maxResults": 1}],
+                }
+            ]
         }
         response = service.execute(body=body)
-        #print(response)
-        if response['responses'][0]:
-            text = response['responses'][0]['textAnnotations'][0]['description']
-            #print('Found text: {}'.format(text))
+        # print(response)
+        if response["responses"][0]:
+            text = response["responses"][0]["textAnnotations"][0]["description"]
+            # print('Found text: {}'.format(text))
         else:
             text = " "
 
     return text
 
-                
 
 logger.info("Converting all the PDF files to JPEG images")
-for pdf in glob.glob('page_*.pdf'):
+for pdf in glob.glob("page_*.pdf"):
     basename = pdf.split(".")[0]
-    pdf_to_jpg = "gs -q -DNOPAUSE -DBATCH -r400 -SDEVICE=jpeg  -sOutputFile=" + basename + ".jpg " + pdf
+    pdf_to_jpg = (
+        GS + " -q -DNOPAUSE -DBATCH -r400 -SDEVICE=jpeg  -sOutputFile="
+        + basename
+        + ".jpg "
+        + pdf
+    )
     logger.info(pdf_to_jpg)
     os.system(pdf_to_jpg)
 
- 
+
 files = []
-for filename in glob.glob('page_*.jpg'):
+for filename in glob.glob("page_*.jpg"):
     files.append(filename)
 
 
-
-
-#Upload the PDF files to google drive and OCR
+# Upload the PDF files to google drive and OCR
 
 upload_counter = 1
 
 for jpg_file in sorted(files):
 
+    if not os.path.isfile(jpg_file.split(".")[0] + ".upload"):
 
-    if not os.path.isfile(jpg_file.split('.')[0] + ".upload"):                        
-                                                
-
-        message= "\n\nuploading " + jpg_file + " to google. "     # File " + str(upload_counter) + " of " + str(len(files)) + " \n\n"
+        message = (
+            "\n\nuploading " + jpg_file + " to google. "
+        )  # File " + str(upload_counter) + " of " + str(len(files)) + " \n\n"
         logger.info(message)
 
-        text = get_text(jpg_file)   
+        text = get_text(jpg_file)
 
         filename = jpg_file.split(".")[0] + ".txt"
 
-        file1=open(filename,"a")
-        #file1.write(text,'\n')
+        file1 = open(filename, "a")
+        # file1.write(text,'\n')
         file1.write("{}\n".format(text))
         file1.close()
-                                                            
-        create_temp_file = "touch " + jpg_file.split('.')[0] + ".upload"
-        logger.info("\n  Creating temp file " + create_temp_file + "\n")
-        os.system(create_temp_file)
-                        
 
-#   	    move_file(pdf_file)
-        logger.info( "\n\n========\n\n")
+        f= open(jpg_file.split(".")[0] + ".upload","w+")
+        f.close()
+        logger.info("Creating temp file "  + "\n")
+        
+
+        #   	    move_file(pdf_file)
+        logger.info("\n\n========\n\n")
         upload_counter = upload_counter + 1
 
 
+jpg_count = len(glob.glob("page_*.jpg"))
+text_count = len(glob.glob("page_*.txt"))
 
-jpg_count = len(glob.glob('page_*.jpg'))
-text_count = len(glob.glob('page_*.txt'))
-
-missing_files = open('missing_files.txt','w')
+missing_files = open("missing_files.txt", "w")
 
 if not jpg_count == text_count:
 
     logger.info("\n\n=========ERROR===========\n\n")
-            
-    for i in range(1,int(jpg_count)+1):
+
+    for i in range(1, int(jpg_count) + 1):
         txt_file = "page_" + str(i).zfill(5) + ".txt"
         if not os.path.isfile(txt_file):
-            missing_files.write(txt_file +"\n")
-            logger.info( "Missing " + txt_file)
-            logger.info( "page_" + str(i).zfill(5) + ".jpg" + " should be reuploaded ")
-                                                                         
+            missing_files.write(txt_file + "\n")
+            logger.info("Missing " + txt_file)
+            logger.info("page_" + str(i).zfill(5) + ".jpg" + " should be reuploaded ")
 
-    logger.info(" \n\nText files are not equal to JPG files. Some JPG files not OCRed. Run this script again to complete OCR all the JPG files \n\n")
+    logger.info(
+        " \n\nText files are not equal to JPG files. Some JPG files not OCRed. Run this script again to complete OCR all the JPG files \n\n"
+    )
     sys.exit()
 
-            
+
 missing_files.close()
-            
+
 
 files = []
-for filename in glob.glob('page_*.txt'):
+for filename in glob.glob("page_*.txt"):
     files.append(filename)
     files.sort()
 
 
+# chunks=[files[x:x+int(columns)] for x in xrange(0, len(files), int(columns))]
 
-                                                
+# counter = 1
 
-#chunks=[files[x:x+int(columns)] for x in xrange(0, len(files), int(columns))]
-
-#counter = 1
-                                               
-#for i in chunks:
+# for i in chunks:
 #    com =  ' '.join(i)
 #    command = "cat  " + com + " > " + "text_for_page_" + str(counter).zfill(5) + ".txt"
 #    counter = counter + 1
@@ -310,20 +318,15 @@ for filename in glob.glob('page_*.txt'):
 #    os.system(command)
 
 
-
-
-
-
-
 files = []
-for textfile in glob.glob('page*.txt'):
+for textfile in glob.glob("page*.txt"):
     files.append(textfile)
     files.sort()
 
-            
-single_file = open("all_text_for_" + filename + ".txt" ,"w")
 
-counter = 1
+single_file = open("all_text_for_" + input_filename + ".txt", "w")
+
+counter = 1 
 for filename in files:
     content = open(filename).read()
     single_file.write("\n\n")
@@ -335,60 +338,52 @@ for filename in files:
     counter = counter + 1
 
 single_file.close()
-                                                                
-
-logger.info("Merged all OCRed files to  all_text_for_" + filename + ".txt")
 
 
-#if not os.path.isdir("text-for-" + filename):
+logger.info("Merged all OCRed files to  all_text_for_" + input_filename + ".txt")
+
+
+# if not os.path.isdir("text-for-" + filename):
 #    os.mkdir("text-for-" + filename)
 
-#command = "cp *.txt text-for-" + filename
-#logger.info("Making a copy of all text files to text-for-" + filename)
-#logger.info("Running " + command.encode('utf-8'))
-#os.system(command.encode('utf-8'))
+# command = "cp *.txt text-for-" + filename
+# logger.info("Making a copy of all text files to text-for-" + filename)
+# logger.info("Running " + command.encode('utf-8'))
+# os.system(command.encode('utf-8'))
 
 
+# message =  "\n\nDone. Check the text files start with text_for_page_ "
+# logger.info(message)
 
 
-
-#message =  "\n\nDone. Check the text files start with text_for_page_ "
-#logger.info(message)
-
-
-
-
-result_text_count = len(glob.glob('page_*.txt'))
+result_text_count = len(glob.glob("page_*.txt"))
 
 if not jpg_count == result_text_count:
     logger.info("\n\n=========ERROR===========\n\n")
-    logger.info(" \n\nText files are not equal to JPG files. Some JPG files not OCRed. Run this script again to complete OCR all the JPG     files \n\n")
+    logger.info(
+        " \n\nText files are not equal to JPG files. Some JPG files not OCRed. Run this script again to complete OCR all the JPG     files \n\n"
+    )
     sys.exit()
 
 
 else:
 
-    message =  "\nMoving all temp files to " + temp_folder + "\n"
+    message = "\nMoving all temp files to " + temp_folder + "\n"
     logger.info(message)
 
-    for file in glob.glob('*.log'):
-        shutil.move(file,temp_folder)
-    
+    for file in glob.glob("*.log"):
+        shutil.move(file, temp_folder)
+
     shutil.move("currentfile.pdf", temp_folder)
 
+    for file in glob.glob("pg*.pdf"):
+        shutil.move(file, temp_folder)
 
-    for file in glob.glob('pg*.pdf'):
-        shutil.move(file,temp_folder)
+    for file in glob.glob("page*"):
+        shutil.move(file, temp_folder)
 
-    for file in glob.glob('page*'):
-        shutil.move(file,temp_folder)
+    for file in glob.glob("txt*"):
+        shutil.move(file, temp_folder)
 
-    for file in glob.glob('txt*'):
-        shutil.move(file,temp_folder)
-
-    for file in glob.glob('*.jpg'):
-        shutil.move(file,temp_folder)
-
-
-
-                                        
+    for file in glob.glob("*.jpg"):
+        shutil.move(file, temp_folder)
