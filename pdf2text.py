45d00d1bf9fb5cad4ab65539ef7c2ff53982d689
Shrinivasan <tshrinivasan@gmail.com>
2019-07-15 19:07:05 +0000

    improved

--- pdf2text.py
+++ pdf2text.py
@@ -13,6 +13,11 @@ import os.path
 import io
 #from utils import Service, encode_image
 import argparse
+import string
+import re
+import telegram_send
+import traceback
+from retrying import retry
 
 from googleapiclient.discovery import build
 from httplib2 import Http
@@ -22,9 +27,6 @@ from apiclient.http import MediaIoBaseDownload, MediaFileUpload
 #parser = argparse.ArgumentParser(description="PDF to Text Convertor", parents=[tools.argparser])
 
 #tools.argparser._action_groups[1].title = 'authentication options'
-
-
-
 
 
 
@@ -42,8 +44,8 @@ version = "1.1"
 #input_filename = args.pdf_file
 #columns = args.columns
 
-
-
+#hostname = "shrini_server"
+	
 config = configparser.ConfigParser()
 config.read("config.ini")
 
@@ -63,6 +65,19 @@ if not os.path.isdir("./log"):
 
 if not os.path.isdir("./completed_source_files"):
     os.mkdir("./completed_source_files")
+
+
+
+
+if not os.path.isdir("./error_1"):
+    os.mkdir("./error_1")
+
+
+
+if not os.path.isdir("./error_2"):
+    os.mkdir("./error_2")
+
+
 
 # create a file handler
 log_file = "./log/pdf2text_" + timestamp + "_log.txt"
@@ -102,6 +117,10 @@ if not float(version) == float(latest_version):
     sys.exit()
 
 
+for temp_folder in glob.glob("OCR*"):
+    shutil.rmtree(temp_folder, ignore_errors=True)
+
+
 logger.info("Running pdf2text.py " + version)
 
 
@@ -139,12 +158,14 @@ columns = config.get("settings", "columns")
 
 #google_vision_api_key = config.get("settings", "google_vision_api_key")
 
-
+hostname =  config.get("settings", "hostname")
 
 MUTOOL = config.get("application_path","mutool")
 PDFSEPARATE = config.get("application_path",  "pdfseparate")
 PDFUNITE = config.get("application_path",  "pdfunite")
 GS = config.get("application_path",  "gs")
+
+
 
 
 if not os.path.isdir(output_folder):
@@ -163,23 +184,141 @@ logger.info("Output Folder name = " + output_folder)
 logger.info("Columns = " + columns)
 
 
+## Renaming the input files by clearing all special charecters
 
-for input_file in glob.glob(input_folder+"/*"):
-    print(input_file)
+delchars = ''.join(c for c in map(chr, range(256)) if not c.isalnum())
 
 
-    input_filename = input_file.split(input_folder+"/")[1] 
+lv_path =  input_folder
+
+
+paths = (os.path.join(root, filename)
+         for root, _, filenames in os.walk(lv_path)
+         for filename in filenames)
+
+print ("Search at " + lv_path)
+
+
+for path in paths:
+    newname = path.replace('#', '')
+    newname = newname.replace('%', '')
+    newname = newname.replace('*', '')
+    newname = newname.replace('<', '')
+    newname = newname.replace('>', '')
+    newname = newname.replace('*', '')
+    newname = newname.replace('?', '')
+    newname = newname.replace('$', '')
+    newname = newname.replace('!', '')
+    newname = newname.replace('\'', '-')
+    newname = newname.replace('"', '')
+    newname = newname.replace('\'', '')
+    newname = newname.replace('Å', 'ś')
+    newname = newname.replace('Å', 'ń')
+    newname = newname.replace(' ', '_')
+    newname = newname.replace("'", '_')
+    newname = newname.replace('(', '_')
+    newname = newname.replace(')', '_')
+    newname = newname.replace('[', '_')
+    newname = newname.replace(']', '_')
+    newname = newname.replace('{', '_')
+    newname = newname.replace('}', '_')
+    newname = newname.replace('`', '_')
+    newname = newname.replace('~', '_')
+    newname = newname.replace('@', '_')
+    newname = newname.replace('}', '_')
+
+    newname = newname.replace('^', '_')
+    newname = newname.replace('&', '_')
+
+    newname = newname.replace('+', '_')
+    newname = newname.replace('=', '_')
+
+    newname = newname.replace('"', '_')
+    newname = newname.replace("'", '_')
+    newname = newname.replace(",", '_')
+
+    newname = newname.replace("\\", '_')
+    newname = newname.replace("|", '_')
+
+
+
+
+    #newname = path.translate(str.maketrans("","",delchars))
+
+    #re.sub('[^\w\-_\. ]', '_', newname)
+
+    if newname != path:
+        # print(path)
+        print(os.path.dirname(path.encode('utf8').decode(sys.stdout.encoding)) + "\t" + os.path.basename(path.encode('utf8').decode(sys.stdout.encoding)) +
+              "\t\t\t -> " + os.path.basename(newname.encode('utf8').decode(sys.stdout.encoding)))
+        os.rename(path, newname)
+
+
+
+telegram_send.send(messages=["Starting OCR on " + hostname])
+
+
+file_number = 0
+total_files = len(glob.glob(input_folder+"/*"))
+
+telegram_send.send(messages=["Total files = " + str(total_files)])
+
+
+#for input_file in glob.glob(input_folder+"/*"):
+
+
+#@retry(stop_max_attempt_number=7, wait_fixed=20000)
+def do_ocr(input_file,temp_folder):
+   
+    # Upload the file on Goggle Drive
+    folder_id = new_folder_id
+    mime = 'application/vnd.google-apps.document'
+    file_metadata = {'name': input_file, 'mimeType': mime, 'parents': [folder_id] }
+    print(file_metadata)
+    media = MediaFileUpload(input_file, mimetype= mime, chunksize=256 * 1024, resumable=True )
+    print(media)
+    Imgfile = service.files().create(body=file_metadata,
+                                            media_body=media,
+                                            fields='id')
+
+
+    response = None
+    while response is None:
+        status, response = Imgfile.next_chunk()
+        time.sleep(1)
+        if status:
+            print("Uploaded %d%%." % int(status.progress() * 100))
+    print("Upload of {} is complete.".format(input_file))
+
+    basename = "".join(os.path.basename(input_file).split(".")[:-1])
+    
+
+    filename = temp_folder + "/" + basename + ".txt"
+    time.sleep(10)
+
+    # Download the file in txt format from Google Drive
+    getTxt = service.files().export_media( fileId = response['id'], mimeType='text/plain')
+    fh = io.FileIO( filename , 'wb' )
+    downloader = MediaIoBaseDownload(fh, getTxt)
+#    downloader.next_chunk()
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+
+
+    
+
+def prepare_file(source_file, split_number):
+
+    global file_number
+
+    file_number = file_number + 1
+
+    input_filename = source_file.split(input_folder+"/")[1] 
     filetype = input_filename.split(".")[-1].lower()
 
     logger.info("File Type = " + filetype)
-
-
-#pdf_dir = working_directory + "/" +  input_filename
-#os.mkdir(pdf_dir)
-#os.chdir(working_directory)
-#shutil.copy(input_filename, pdf_dir +"/" + input_filename)
-
-
 
 
     temp_folder = "OCR-" + input_filename + "-temp-" + timestamp
@@ -191,144 +330,52 @@ for input_file in glob.glob(input_folder+"/*"):
 
     if filetype.lower() == "pdf":
 
-    # split the PDF files vertically based on the column numbers
-
-        message = "Aligining the Pages of PDF file. \n"
-        logger.info(message)
-        command = (
-        MUTOOL + " poster -x "
-        + str(columns)
-        + " "
-        + '"'
-        + input_file
-        + '" '
-        + temp_folder + "/currentfile.pdf"
-        )
-        logger.info("Running " + command)
-
-        os.system(command.encode("utf-8"))
-
-    # https://blog.alivate.com.au/poppler-windows/
-
-        message = "Spliting the PDF into single pages. \n"
-        logger.info(message)
-        burst_command = PDFSEPARATE +   " " + temp_folder + "/currentfile.pdf " + temp_folder + "/pg-%05d.pdf"
-        os.system(burst_command)
-        logger.info("Running " + burst_command)
-
-        files = []
-        for filename in glob.glob(temp_folder + "/" + "pg*.pdf"):
-            files.append(filename)
-            files.sort()
-
-        chunks = [files[x : x + int(columns)] for x in range(0, len(files), int(columns))]
-
-        counter = 1
-        message = "Joining the PDF files ...\n"
-        logger.info(message)
-
-        if columns == "1":
-            counter = 1
-            for pdf in files:
-                shutil.copy(pdf, temp_folder +"/" + "page_" + str(counter).zfill(5) + ".pdf")
-            # command = "cp " + pdf +  " page_" + str(counter).zfill(5) + ".pdf"
-            # logger.info("Running Command " + command)
-                counter = counter + 1
-            # os.system(command)
-
-        if columns == "2":
-
-            chunks = [
-                files[x : x + int(columns)] for x in range(0, len(files), int(columns))
-            ]
-
-      
-            counter = 1
-            message = "Joining the PDF files ...\n"
-            logger.info(message)
-
-            for i in chunks:
-                com = " ".join(i)
-                command = PDFUNITE + " " + com + " " +  temp_folder +"/" + "page_" + str(counter).zfill(5) + ".pdf"
-                logger.info("Running " + command)
-                counter = counter + 1
-                os.system(command)
 
 
-
-
-
-
-    logger.info("Converting all the PDF files to JPEG images")
-    for pdf in glob.glob(temp_folder +"/" + "page_*.pdf"):
-
-        print(pdf)
-        basename = "".join(os.path.basename(pdf).split(".")[:-1])
-        print(basename)
-        pdf_to_jpg = (
-            GS + " -q -DNOPAUSE -DBATCH -r800 -SDEVICE=jpeg  -sOutputFile="
-            + temp_folder + "/" + basename
-            + ".jpg "
-            + pdf
-        )
-
-        logger.info(pdf_to_jpg)
-        os.system(pdf_to_jpg)
-
+        split_command = "qpdf --split-pages=" + str(split_number) + " " + source_file + " " + temp_folder + "/pg_%d.pdf"
+        logging.info("Spliting the PDF file")
+        logging.info(split_command)
+        os.system(split_command)
 
     files = []
-    for filename in glob.glob(temp_folder + "/" + "page_*.jpg"):
+    for filename in glob.glob(temp_folder + "/" + "pg_*.pdf"):
         files.append(filename)
 
-
+#    print(files)
 
 
 # Upload the PDF files to google drive and OCR
 
     upload_counter = 1
 
-    for jpg_file in sorted(files):
-        basename = "".join(os.path.basename(jpg_file).split(".")[:-1])
+    for pdf_file in sorted(files):
+        
+        basename = "".join(os.path.basename(pdf_file).split(".")[:-1])
     
         if not os.path.isfile(temp_folder + "/" + basename + ".upload"):
 
             message = (
-                "\n\nuploading " + jpg_file + " to google. "
+                "\n\nuploading " + pdf_file + " to google.   " + str(file_number) +"/" + str(total_files)
             )  # File " + str(upload_counter) + " of " + str(len(files)) + " \n\n"
             logger.info(message)
 
 
+            try:
 
-        # Upload the file on Goggle Drive
-            folder_id = new_folder_id
-            mime = 'application/vnd.google-apps.document'
-            file_metadata = {'name': jpg_file, 'mimeType': mime, 'parents': [folder_id] }
-            print(file_metadata)
-            media = MediaFileUpload( jpg_file, mimetype= mime )
-            print(media)
-            Imgfile = service.files().create(body=file_metadata,
-                                            media_body=media,
-                                            fields='id').execute()
+                do_ocr(pdf_file, temp_folder)
+            except Exception as e:
+                print(e)
+                print(traceback.format_exc())
+                telegram_send.send(messages=["ALERT : Error on " + filename + ". Script Stopped at " + hostname +". Rerun again"])
+#                sys.exit()
+#                os.system("python3 pdf2text.py")
 
+                error_message = "Got issues on " + source_file + " " + str(file_number) +"/" + str(total_files) + " on " + hostname
+                telegram_send.send(messages=[error_message])
+                traceback_text = traceback.format_exc()
+                message = "Error on " + __file__ +  "\n" + traceback_text
+                telegram_send.send(messages=[error_message])
 
-            filename = temp_folder + "/" + basename + ".txt"
-
-        # Download the file in txt format from Google Drive
-            getTxt = service.files().export_media( fileId = Imgfile.get('id'), mimeType='text/plain')
-            fh = io.FileIO( filename , 'wb' )
-            downloader = MediaIoBaseDownload(fh, getTxt)
-            downloader.next_chunk()
-
-
-
-        #text = get_text(jpg_file)
-
-
-
-        #file1 = open(filename, "a")
-        # file1.write(text,'\n')
-        #file1.write("{}\n".format(text))
-        #file1.close()
 
             f= open(temp_folder + "/" + basename + ".upload","w+")
             f.close()
@@ -340,99 +387,110 @@ for input_file in glob.glob(input_folder+"/*"):
             upload_counter = upload_counter + 1
 
 
-    jpg_count = len(glob.glob(temp_folder + "/" + "page_*.jpg"))
-    text_count = len(glob.glob(temp_folder + "/"  + "page_*.txt"))
+    pdf_count = len(glob.glob(temp_folder + "/" + "pg_*.pdf"))
+    text_count = len(glob.glob(temp_folder + "/"  + "pg_*.txt"))
 
     missing_files = open("missing_files.txt", "w")
 
-    if not jpg_count == text_count:
+    if not pdf_count == text_count:
 
         logger.info("\n\n=========ERROR===========\n\n")
 
-        for i in range(1, int(jpg_count) + 1):
+        for i in range(1, int(pdf_count) + 1):
             txt_file = "page_" + str(i).zfill(5) + ".txt"
             if not os.path.isfile(txt_file):
                 missing_files.write(txt_file + "\n")
                 logger.info("Missing " + txt_file)
-                logger.info("page_" + str(i).zfill(5) + ".jpg" + " should be reuploaded ")
+                logger.info("pg_" + str(i).zfill(5) + ".pdf" + " should be reuploaded ")
 
         logger.info(
             " \n\nText files are not equal to JPG files. Some JPG files not OCRed. Run this script again to complete OCR all the JPG files \n\n"
         )
-        sys.exit()
+
+#	os.system("telegram-send " + "FAIL : Error on " + input_filename)
+        telegram_send.send(messages=["FAIL : Error on " + input_filename])
+	
 
 
     missing_files.close()
 
-
+    '''
     files = []
-    for filename in glob.glob(temp_folder + "/" + "page_*.txt"):
-        files.append(filename)
-        files.sort()
-
-
-# chunks=[files[x:x+int(columns)] for x in xrange(0, len(files), int(columns))]
-
-# counter = 1
-
-# for i in chunks:
-#    com =  ' '.join(i)
-#    command = "cat  " + com + " > " + "text_for_page_" + str(counter).zfill(5) + ".txt"
-#    counter = counter + 1
-#    logger.info("Running " + command)
-#    os.system(command)
-
-
-    files = []
-    for textfile in glob.glob(temp_folder  +"/" + "page*.txt"):
+    for textfile in glob.glob(temp_folder  +"/" + "pg*.txt"):
         files.append(textfile)
         files.sort()
 
 
     single_file = open("all_text_for_" + input_filename + ".txt", "w")
 
-    counter = 1 
+#    counter = 1 
     for filename in files:
         content = open(filename).read()
-        single_file.write("\n\n")
-        single_file.write("Page " + str(counter))
+#        single_file.write("\n\n")
+#        single_file.write("Page " + str(counter))
         single_file.write("\n\n")
         single_file.write(content)
         single_file.write("\n\n")
-        single_file.write("xxxxxxxxxx")
-        counter = counter + 1
+#        single_file.write("xxxxxxxxxx")
+#        counter = counter + 1
 
     single_file.close()
 
     shutil.move("all_text_for_" + input_filename + ".txt", output_folder + "/" + "all_text_for_" + input_filename + ".txt" )
     
     logger.info("Merged all OCRed files to  all_text_for_" + input_filename + ".txt")
+    '''
 
+    result_text_count = len(glob.glob(temp_folder + "/" + "pg_*.txt"))
 
-# if not os.path.isdir("text-for-" + filename):
-#    os.mkdir("text-for-" + filename)
-
-# command = "cp *.txt text-for-" + filename
-# logger.info("Making a copy of all text files to text-for-" + filename)
-# logger.info("Running " + command.encode('utf-8'))
-# os.system(command.encode('utf-8'))
-
-
-# message =  "\n\nDone. Check the text files start with text_for_page_ "
-# logger.info(message)
-
-
-    result_text_count = len(glob.glob(temp_folder + "/" + "page_*.txt"))
-
-    if not jpg_count == result_text_count:
+    if not pdf_count == result_text_count:
         logger.info("\n\n=========ERROR===========\n\n")
         logger.info(
-        " \n\nText files are not equal to JPG files. Some JPG files not OCRed. Run this script again to complete OCR all the JPG     files \n\n"
+        " \n\nText files are not equal to PDF files. Some PDF files not OCRed. Run this script again to complete OCR all the PDF files \n\n"
         )
-        sys.exit()
+	#os.system("telegram-send " + "FAIL : Error on " + input_filename)
+        telegram_send.send(messages=["FAIL : Error on " + source_file])
+#        sys.exit()
+#        os.system("python3 pdf2text.py")
+
+        
+        if "input" in source_file:
+            shutil.move(source_file, "./error_1/" )
+        
+        if "error_1" in source_file:
+            shutil.move(source_file, "./error_2/" )
 
 
     else:
+
+
+        files = []
+        for textfile in glob.glob(temp_folder  +"/" + "pg*.txt"):
+            files.append(textfile)
+            files.sort()
+
+
+        single_file = open("all_text_for_" + input_filename + ".txt", "w")
+
+#    counter = 1 
+        for filename in files:
+            content = open(filename).read()
+#        single_file.write("\n\n")
+#        single_file.write("Page " + str(counter))
+            single_file.write("\n\n")
+            single_file.write(content)
+            single_file.write("\n\n")
+#        single_file.write("xxxxxxxxxx")
+#        counter = counter + 1
+
+        single_file.close()
+
+        shutil.move("all_text_for_" + input_filename + ".txt", output_folder + "/" + "all_text_for_" + input_filename + ".txt" )
+    
+        logger.info("Merged all OCRed files to  all_text_for_" + input_filename + ".txt")
+
+
+
 
         message = "\nMoving all temp files to " + temp_folder + "\n"
         logger.info(message)
@@ -440,23 +498,44 @@ for input_file in glob.glob(input_folder+"/*"):
         for file in glob.glob("*.log"):
             shutil.move(file, temp_folder)
 
-    #shutil.move("currentfile.pdf", temp_folder)
+        shutil.move(source_file, "./completed_source_files/" )
 
-    #for file in glob.glob("pg*.pdf"):
-    #    shutil.move(file, temp_folder)
+        telegram_send.send(messages=["PASS : OCR Done on " + source_file + " "  + "Result file = all_text_for_" + source_file + ".txt " + str(file_number) +"/" + str(total_files) + " on " + hostname])
 
-    #for file in glob.glob("page*"):
-    #    shutil.move(file, temp_folder)
+        shutil.rmtree(temp_folder, ignore_errors=True)
 
-    #for file in glob.glob("txt*"):
-    #    shutil.move(file, temp_folder)
 
-    #for file in glob.glob("*.jpg"):
-    #    shutil.move(file, temp_folder)
 
-    shutil.move(input_file, "./completed_source_files/" )
+try:
+    for source_file in glob.glob(input_folder+"/*"):
+        prepare_file(source_file, 20)
+
+
+    for source_file in glob.glob("error_1/*"):
+        prepare_file(source_file, 1)
+
+
+
+
+except Exception as e:
+    print(e)
+    print(traceback.format_exc())
+    error_message = "Got issues on " + source_file + " " + str(file_number) +"/" + str(total_files) + " on " + hostname
+    telegram_send.send(messages=[error_message])
+    traceback_text = traceback.format_exc()
+    message = "Error on " + __file__ +  "\n" + traceback_text
+    telegram_send.send(messages=[error_message])
+
+#    os.system("python3 pdf2text.py")
+
+
 
 service.files().delete(fileId=new_folder_id).execute()
 
-for temp_folder in glob.glob("OCR*"):
-    shutil.rmtree(temp_folder, ignore_errors=True)
+
+
+#for temp_folder in glob.glob("OCR*"):
+#    shutil.rmtree(temp_folder, ignore_errors=True)
+
+
+telegram_send.send(messages=["All files done on " + hostname + ". Add next batch"])    
